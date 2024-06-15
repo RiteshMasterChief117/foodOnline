cbecbe4a5e47bdd24316cf80f631915a61546a5f
RiteshMasterChief117 <riteshdutta117@gmail.com>
2024-06-16 03:12:14 +0530

    user registration

--- accounts/views.py
+++ accounts/views.py
@@ -3,21 +3,38 @@ from django.shortcuts import render, redirect
 
 from accounts.forms import UserForm
 from .models import User
-
+from django.contrib import messages
 # Create your views here.
 def registerUser(request):
     if request.method == 'POST':
         print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
+            #create user using forms
+            # password = form.cleaned_data['password']
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # user.save()
+
+            # Create USer using create user method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email,
+                                            password=password)
             user.role = User.CUSTOMER
             user.save()
+            print("user is created from registerUser")
+            messages.success(request,'Your Account has been registered successfully')
             return redirect('registerUser')
+        else:
+            print('invalid form')
+            print(form.errors)
     else:
         form = UserForm()
-
-    form = UserForm()
     context = {
         'form':form,
     }
